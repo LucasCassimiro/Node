80b8216678d9a8658a70827b2d80e7f08a03d058
Lucas Cassimiro <lucas.cassimir@gmail.com>
2020-07-07 14:29:43 -0300

    Padrão RestFull, Hapi e Joi

---
+++ 14-hapi-listar/src/tests/apiHeroes.test.js
@@ -0,0 +1,79 @@
+// const assert = require('assert')
+// const api = require('./../api')
+// let app = {}
+
+// describe.only('Suite de testes da API Heroes', function () {
+//     this.beforeAll(async () => {
+//         app = await api
+//     })
+
+//     it('Listar /herois', async () => {
+//         const result = await app.inject({
+//             method: 'GET',
+//             url: '/herois'
+//         })
+
+//         const dados = JSON.parse(result.payload)
+//         const statusCode = result.statusCode
+
+//         assert.deepEqual(statusCode, 200)
+//         assert.ok(Array.isArray(dados))
+//     })
+//     it('Listar /heroes - deve retornar somente 3 registros', async () => {
+//         const TAMANHO_LIMITE = 3
+//         const result = await app.inject({
+//             method: 'GET',
+//             url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+//         })
+
+//         const dados = JSON.parse(result.payload)
+//         console.log('Dados', dados.length)
+//         const statusCode = result.statusCode
+//         assert.deepEqual(statusCode, 200)
+//         assert.ok(dados.length === TAMANHO_LIMITE)
+//     })
+
+//     it('Listar /heroes - deve retornar somente 3 registros', async () => {
+//         const TAMANHO_LIMITE = 'AEEE'
+//         const result = await app.inject({
+//             method: 'GET',
+//             url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+//         })
+
+//         // const dados = JSON.parse(result.payload)
+//         assert.deepEqual(result.payload, 'Erro interno no servidor')
+
+//     })
+//     it('Listar /heroes - deve filtrar um item', async () => {
+//         const NAME = 'Homem Aranha'
+//         const result = await app.inject({
+//             method: 'GET',
+//             url: `/herois?skip=0&limit=100&nome=${NAME}`
+//         })
+
+//         const dados = JSON.parse(result.payload)
+//         const statusCode = result.statusCode
+//         assert.deepEqual(statusCode, 200)
+//         assert.deepEqual(dados[0].nome, NAME)
+//     })
+// })
+
+const assert = require('assert')
+const api = require('./../api')
+let app = {}
+describe('API Heroes test suite', function ()  {
+    this.beforeAll(async () => {
+        app = await api
+    })
+    it('listar /heroes', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/herois'
+        })
+        const statusCode = result.statusCode 
+        
+        assert.deepEqual(statusCode, 200)
+        assert.ok(Array.isArray(JSON.parse(result.payload)))
+    })
+
+})
---
+++ 15-hapi-listar-joi/src/tests/apiHeroes.test.js
@@ -0,0 +1,82 @@
+const assert = require('assert')
+const api = require('./../api')
+let app = {}
+
+describe.only('Suite de testes da API Heroes', function () {
+    this.beforeAll(async () => {
+        app = await api
+    })
+
+    it('Listar /herois', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/herois'
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+
+        assert.deepEqual(statusCode, 200)
+        assert.ok(Array.isArray(dados))
+    })
+    it('Listar /heroes - deve retornar somente 3 registros', async () => {
+        const TAMANHO_LIMITE = 3
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        console.log('Dados', dados.length)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados.length === TAMANHO_LIMITE)
+    })
+
+    it('Listar /heroes - deve retornar um erro com limit incorreto', async () => {
+        const TAMANHO_LIMITE = 'AEEE'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+        const errorResult = {
+            "statusCode":400,"error":"Bad Request","message":"child \"limit\" fails because [\"limit\" must be a number]","validation":{"source":"query","keys":["limit"]
+            }
+        }
+        assert.deepEqual(result.statusCode, 400)
+        assert.deepEqual(result.payload, JSON.stringify(errorResult))
+
+    })
+    it('Listar /heroes - deve filtrar um item', async () => {
+        const NAME = 'Homem Aranha-1593546620547'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=100&nome=${NAME}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.deepEqual(dados[0].nome, NAME)
+    })
+})
+
+// const assert = require('assert')
+// const api = require('./../api')
+// let app = {}
+// describe('API Heroes test suite', function ()  {
+//     this.beforeAll(async () => {
+//         app = await api
+//     })
+//     it('listar /heroes', async () => {
+//         const result = await app.inject({
+//             method: 'GET',
+//             url: '/herois'
+//         })
+//         const statusCode = result.statusCode 
+        
+//         assert.deepEqual(statusCode, 200)
+//         assert.ok(Array.isArray(JSON.parse(result.payload)))
+//     })
+
+// })
